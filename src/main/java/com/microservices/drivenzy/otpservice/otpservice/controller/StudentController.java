ddfbdc335c7843b633129b7370e41a532fc97971
rishikeshav.kh <rishikeshav.kh>
2024-05-05 10:50:41 +0530

    added Student for plan b gym

--- src/main/java/com/microservices/drivenzy/otpservice/otpservice/controller/StudentController.java
+++ src/main/java/com/microservices/drivenzy/otpservice/otpservice/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/student")
