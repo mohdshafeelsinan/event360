0a17d815e22c924d9162561ee033b90b4ae70a60
jkjoseph2023@gmail.com <jkjoseph2023@gmail.com>
2024-06-01 16:32:10 +0530

    OK

--- src/main/java/com/microservices/drivenzy/otpservice/otpservice/modal/Student.java
+++ src/main/java/com/microservices/drivenzy/otpservice/otpservice/modal/Student.java
@@ -3,6 +3,9 @@ package com.microservices.drivenzy.otpservice.otpservice.modal;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 @Document("Student")
 public class Student {
@@ -13,11 +16,22 @@ public class Student {
     private String email;
     private String socialMedia;
     private String studentId;
-    private String emiritesOrPassportNo;
+    private String emiratesOrPassportNo;
     private String healthIssue;
-    private String dob;
     private String gender;
     private String image;
     private String address;
     private Boolean status;
+    private Long phoneNumber;
+    private Long whatsappNumber;
+    private String id;
+    private LocalDate dob; // Using LocalDate for date of birth
+    private List<String> couresEnrolled;
+    private List<String> classDate; // Assuming these are dates, using List<String> for simplicity
+    private List<String> trainer;
+    private Long fees;
+    private Long vat;
+    private String paymentMode;
+    private LocalDate paymentDate;
+    private String addNotes;
 }
